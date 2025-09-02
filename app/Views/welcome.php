455f383b34d609a320c40c544300d733212a92a9
inovafani <inoveka@gmail.com>
2025-09-03 02:55:26 +0700

    Finish

--- app/Views/welcome.php
+++ app/Views/welcome.php
@@ -1,6 +1,6 @@
 <?php echo $this->extend('layouts/main') ?>
 <?php echo $this->section('content') ?>
-    <div class="p-5 text-center bg-white rounded-3">
+    <div class="p-5 text-center bg-white rounded-3 shadow-sm">
         <h1 class="text-body-emphasis">Aplikasi CRUD Pegawai</h1>
         <p class="lead">
             Selamat datang di aplikasi CRUD Pegawai sederhana menggunakan CodeIgniter 4. Aplikasi ini memungkinkan Anda untuk mengelola data pegawai dan jabatan dengan mudah.
